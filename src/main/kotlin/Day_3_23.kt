24051c068bd4ba2c9750e26c0e2b65b240ef2b83
caojun05 <caojun05@kuaishou.com>
2022-03-23 21:12:13 +0800

    白天三题，晚上两题

--- src/main/kotlin/Day_3_23.kt
+++ src/main/kotlin/Day_3_23.kt
@@ -1,3 +1,7 @@
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.roundToInt
+
 class Day_3_23 {
     // search in a rotated array
     fun search(nums: IntArray, target: Int): Int {
@@ -35,7 +39,7 @@ class Day_3_23 {
             return intArrayOf(-1, -1)
         }
     }
-    fun searchLeftBound(nums: IntArray, target: Int): Int{
+    private fun searchLeftBound(nums: IntArray, target: Int): Int{
         var left = 0
         var right = nums.size - 1
         while(left <= right){
@@ -50,7 +54,7 @@ class Day_3_23 {
         return left
 
     }
-    fun searchRightBound(nums: IntArray, target: Int): Int{
+    private fun searchRightBound(nums: IntArray, target: Int): Int{
         var left = 0
         var right = nums.size - 1
         while(left <= right){
@@ -63,4 +67,117 @@ class Day_3_23 {
         }
         return right
     }
+
+    //lt 39
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        val res = ArrayList<List<Int>>()
+        combinationSumBacktrack(
+            candidates,
+            res,
+            ArrayList(),
+            target,
+            0
+        )
+        return res
+    }
+    fun combinationSumBacktrack(
+        candidates: IntArray,
+        allList: ArrayList<List<Int>>,
+        tempList: ArrayList<Int>,
+        remain: Int,
+        start: Int
+    ){
+        if (remain < 0){
+            return
+        }else if (remain == 0){
+            allList.add(ArrayList(tempList))
+        }else{
+            for (i in start until candidates.size) {
+                tempList.add(candidates[i])
+                combinationSumBacktrack(candidates, allList, tempList, remain - candidates[i], i) // not i + 1 because we can reuse same elements
+                tempList.removeAt(tempList.size - 1)
+            }
+        }
+    }
+
+
+    fun permute(nums: IntArray): List<List<Int>> {
+        val track = ArrayList<Int>()
+        val res = ArrayList<List<Int>>()
+        fun permuteBacktrack(){
+            if (track.size == nums.size){
+                res.add(ArrayList(track))
+                return
+            }
+            for (i in nums.indices){
+                if (track.contains(nums[i])){
+                    continue
+                }else{
+                    track.add(nums[i])
+                    permuteBacktrack()
+                    track.removeLast()
+                }
+            }
+        }
+        permuteBacktrack()
+        return res
+    }
+
+
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        val track = ArrayList<Int>()
+        val res = ArrayList<List<Int>>()
+        val visited = BooleanArray(nums.size) { false }
+        fun permuteBacktrack(){
+            if (track.size == nums.size){
+                res.add(ArrayList(track))
+                return
+            }
+            for (i in nums.indices){
+                //when a number has the same value with its previous, we can use this number only if his previous is used
+                if (visited[i] || (i > 0 && nums[i-1] == nums[i] && !visited[i-1])){
+                    continue
+                }else{
+                    visited[i] = true
+                    track.add(nums[i])
+                    permuteBacktrack()
+                    track.removeLast()
+                    visited[i] = false
+                }
+            }
+        }
+        Arrays.sort(nums)
+        permuteBacktrack()
+        return res
+    }
+
+    fun uniquePaths(m: Int, n: Int): Int {
+        var bottom = m + n -2
+        var up = kotlin.math.min(m-1, n-1)
+        var res: Double = 1.0
+        while (up > 0){
+            res *= bottom
+            res /= up
+            bottom--
+            up--
+        }
+        return kotlin.math.round(res).toInt()
+    }
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+        var pointer = 0
+        for (i in 1 until nums.size ){
+            if (nums[pointer] != nums[i]){
+                pointer += 1
+                nums[pointer] = nums[i]
+            }
+        }
+        return pointer + 1
+    }
+}
+fun main(){
+//    println(Day_3_23().permuteUnique(intArrayOf(1,2,2,3)))
+    println(Day_3_23().uniquePaths(4, 6))
+    println(Day_3_23().removeDuplicates(intArrayOf(1,2,2,3,3,4,5,5)))
+
 }
